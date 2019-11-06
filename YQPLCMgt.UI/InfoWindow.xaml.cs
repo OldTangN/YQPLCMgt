8d3b52280ea26278c2bbec6b683aea3b31157311
tangning <tangning@163.com>
2019-11-06 09:20:23 +0800

    设备消息窗口上传不上传配置保存到文本文件

--- YQPLCMgt.UI/InfoWindow.xaml.cs
+++ YQPLCMgt.UI/InfoWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using YQPLCMgt.Helper;
+using YQPLCMgt.UI.ViewModel;
 
 namespace YQPLCMgt.UI
 {
@@ -41,6 +43,88 @@ namespace YQPLCMgt.UI
                     }
                 }
             }
+        }
+
+        private string cfgFileName = "DeviceEnable.txt";
+        private void SaveCfg()
+        {
+            try
+            {
+                FileStream fs = new FileStream(cfgFileName, FileMode.Create);
+                StreamWriter writer = new StreamWriter(fs);
+                foreach (var dev in viewModel.Source.ScanDevices)
+                {
+                    writer.WriteLine($"{dev.NO},{dev.Enable}");
+                }
+                foreach (var dev in viewModel.Source.StopDevices)
+                {
+                    writer.WriteLine($"{dev.NO},{dev.Enable}");
+                }
+                foreach (var dev in viewModel.Source.MachineDevices)
+                {
+                    writer.WriteLine($"{dev.NO},{dev.Enable}");
+                }
+                writer.Close();
+                writer.Dispose();
+                fs.Close();
+                fs.Dispose();
+            }
+            catch (Exception)
+            {
+            }
+        }
+        private void ReadCfg()
+        {
+            try
+            {
+                FileStream fs = new FileStream(cfgFileName, FileMode.OpenOrCreate);
+                StreamReader reader = new StreamReader(fs);
+                while (!reader.EndOfStream)
+                {
+                    string data = reader.ReadLine();
+                    if (string.IsNullOrEmpty(data))
+                    {
+                        continue;
+                    }
+                    try
+                    {
+                        string[] arrData = data.Split(',');
+                        var scan = this.viewModel.Source.ScanDevices.FirstOrDefault(p => p.NO == arrData[0]);
+                        if (scan != null)
+                        {
+                            scan.Enable = Convert.ToBoolean(arrData[1]);
+                        }
+                        var stop = this.viewModel.Source.StopDevices.FirstOrDefault(p => p.NO == arrData[0]);
+                        if (stop != null)
+                        {
+                            stop.Enable = Convert.ToBoolean(arrData[1]);
+                        }
+                        var machine = this.viewModel.Source.MachineDevices.FirstOrDefault(p => p.NO == arrData[0]);
+                        if (machine != null)
+                        {
+                            machine.Enable = Convert.ToBoolean(arrData[1]);
+                        }
+                    }
+                    catch (Exception)
+                    {
+                    }
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            SaveCfg();
+        }
+
+        private MainViewModel viewModel;
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            viewModel = DataContext as MainViewModel;
+            ReadCfg();
         }
     }
 }
